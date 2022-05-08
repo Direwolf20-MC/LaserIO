55a8e13356273a6cfb58838b7fe62accb08e31d2
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2022-05-08 14:43:08 -0400

    Overclocker Items Added

---
+++ src/main/java/com/direwolf20/laserio/common/items/upgrades/OverclockerCard.java
@@ -0,0 +1,10 @@
+package com.direwolf20.laserio.common.items.upgrades;
+
+import com.direwolf20.laserio.setup.ModSetup;
+import net.minecraft.world.item.Item;
+
+public class OverclockerCard extends Item {
+    public OverclockerCard() {
+        super(new Item.Properties().tab(ModSetup.ITEM_GROUP));
+    }
+}
--- src/main/java/com/direwolf20/laserio/datagen/LaserIOLanguageProvider.java
+++ src/main/java/com/direwolf20/laserio/datagen/LaserIOLanguageProvider.java
@@ -26,6 +26,8 @@ public class LaserIOLanguageProvider extends LanguageProvider {
         add(Registration.Filter_Tag.get(), "Tag Filter");
         add(Registration.Logic_Chip.get(), "Logic Chip");
         add(Registration.Logic_Chip_Raw.get(), "Raw Logic Chip");
+        add(Registration.Overclocker_Card.get(), "Card Overclocker");
+        add(Registration.Overclocker_Node.get(), "Node Overclocker");
 
         add("screen.laserio.extractamt", "Extract");
         add("screen.laserio.priority", "Priority");
