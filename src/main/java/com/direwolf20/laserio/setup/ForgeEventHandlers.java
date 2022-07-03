5de55a314c422c768fd08d80c2e2829a35aab382
jollycar <jollycar@github.com>
2022-07-03 22:47:58 +0200

    Cancel rightclick from offhand
    When clicking the wrench on any BaseLaserBlock,
    while items(s) in offhand,
    do not allow the event to proceed.

--- src/main/java/com/direwolf20/laserio/setup/ForgeEventHandlers.java
+++ src/main/java/com/direwolf20/laserio/setup/ForgeEventHandlers.java
@@ -1,18 +1,34 @@
 package com.direwolf20.laserio.setup;
 
+import com.direwolf20.laserio.common.blocks.baseblocks.BaseLaserBlock;
+import com.direwolf20.laserio.common.items.LaserWrench;
+import com.direwolf20.laserio.util.VectorHelper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static com.direwolf20.laserio.util.VectorHelper.RANGE;
+import static net.minecraft.world.InteractionHand.OFF_HAND;
 
 public class ForgeEventHandlers {
 
     @SubscribeEvent
-    public void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
-        // TODO: implement
-    }
-
-    @SubscribeEvent
     public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-        // TODO: implement
+        Level world = event.getWorld();
+        Player player = event.getPlayer();
+        ItemStack heldItem = player.getMainHandItem();
+
+        if (event.getHand().equals(OFF_HAND)) {
+            boolean hasItemInOffhand = !player.getOffhandItem().isEmpty();
+            boolean isWrenchHeld = !heldItem.isEmpty() && heldItem.getItem() instanceof LaserWrench;
+            boolean isLookingAtBaseLaserBlock = world.getBlockState(VectorHelper.getLookingAt(player, RANGE).getBlockPos()).getBlock() instanceof BaseLaserBlock;
+
+            if (hasItemInOffhand && isWrenchHeld && isLookingAtBaseLaserBlock) {
+                event.setCanceled(true);
+            }
+        }
     }
 
 }
