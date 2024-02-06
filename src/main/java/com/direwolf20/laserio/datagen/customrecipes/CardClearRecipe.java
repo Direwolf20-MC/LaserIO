442b51b1d80d614af8c010c2bb7c4e3cc2f6d2a5
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-02-06 12:03:03 -0500

    Fix card clearing recipes

--- src/main/java/com/direwolf20/laserio/datagen/customrecipes/CardClearRecipe.java
+++ src/main/java/com/direwolf20/laserio/datagen/customrecipes/CardClearRecipe.java
@@ -2,25 +2,90 @@ package com.direwolf20.laserio.datagen.customrecipes;
 
 import com.direwolf20.laserio.common.items.cards.BaseCard;
 import com.direwolf20.laserio.setup.Registration;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.ShapelessRecipe;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
-public class CardClearRecipe extends ShapelessRecipe {
+public class CardClearRecipe implements CraftingRecipe {
+    final String group;
+    final CraftingBookCategory category;
+    final ItemStack result;
+    final NonNullList<Ingredient> ingredients;
+    private final boolean isSimple;
+
     public CardClearRecipe(String pGroup, CraftingBookCategory pCategory, ItemStack pResult, NonNullList<Ingredient> pIngredients) {
-        super(pGroup, pCategory, pResult, pIngredients);
+        this.group = pGroup;
+        this.category = pCategory;
+        this.result = pResult;
+        this.ingredients = pIngredients;
+        this.isSimple = pIngredients.stream().allMatch(Ingredient::isSimple);
+    }
+
+    @Override
+    public String getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public CraftingBookCategory category() {
+        return this.category;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess pRegistryAccess) {
+        return this.result;
+    }
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        return this.ingredients;
     }
 
     @Override
     public boolean isSpecial() {
         return true;
+    }
+
+    public boolean matches(CraftingContainer pInv, Level pLevel) {
+        StackedContents stackedcontents = new StackedContents();
+        java.util.List<ItemStack> inputs = new java.util.ArrayList<>();
+        int i = 0;
+
+        for (int j = 0; j < pInv.getContainerSize(); ++j) {
+            ItemStack itemstack = pInv.getItem(j);
+            if (!itemstack.isEmpty()) {
+                ++i;
+                if (isSimple)
+                    stackedcontents.accountStack(itemstack, 1);
+                else inputs.add(itemstack);
+            }
+        }
+
+        return i == this.ingredients.size() && (isSimple ? stackedcontents.canCraft(this, null) : net.neoforged.neoforge.common.util.RecipeMatcher.findMatches(inputs, this.ingredients) != null);
+    }
+
+    public ItemStack assemble(CraftingContainer pContainer, RegistryAccess pRegistryAccess) {
+        return this.result.copy();
+    }
+
+    /**
+     * Used to determine if this recipe can fit in a grid of the given width/height
+     */
+    @Override
+    public boolean canCraftInDimensions(int pWidth, int pHeight) {
+        return pWidth * pHeight >= this.ingredients.size();
     }
 
     @Override
@@ -54,7 +119,64 @@ public class CardClearRecipe extends ShapelessRecipe {
     }
 
 
-    public static class Serializer extends ShapelessRecipe.Serializer {
+    public static class Serializer implements RecipeSerializer<CardClearRecipe> {
+        private static final net.minecraft.resources.ResourceLocation NAME = new net.minecraft.resources.ResourceLocation("laserio", "cardclear");
+        private static final Codec<CardClearRecipe> CODEC = RecordCodecBuilder.create(
+                p_311734_ -> p_311734_.group(
+                                ExtraCodecs.strictOptionalField(Codec.STRING, "group", "").forGetter(p_301127_ -> p_301127_.group),
+                                CraftingBookCategory.CODEC.fieldOf("category").orElse(CraftingBookCategory.MISC).forGetter(p_301133_ -> p_301133_.category),
+                                ItemStack.ITEM_WITH_COUNT_CODEC.fieldOf("result").forGetter(p_301142_ -> p_301142_.result),
+                                Ingredient.CODEC_NONEMPTY
+                                        .listOf()
+                                        .fieldOf("ingredients")
+                                        .flatXmap(
+                                                p_301021_ -> {
+                                                    Ingredient[] aingredient = p_301021_
+                                                            .toArray(Ingredient[]::new); //Forge skip the empty check and immediatly create the array.
+                                                    if (aingredient.length == 0) {
+                                                        return DataResult.error(() -> "No ingredients for shapeless recipe");
+                                                    } else {
+                                                        return aingredient.length > ShapedRecipePattern.getMaxHeight() * ShapedRecipePattern.getMaxWidth()
+                                                                ? DataResult.error(() -> "Too many ingredients for shapeless recipe. The maximum is: %s".formatted(ShapedRecipePattern.getMaxHeight() * ShapedRecipePattern.getMaxWidth()))
+                                                                : DataResult.success(NonNullList.of(Ingredient.EMPTY, aingredient));
+                                                    }
+                                                },
+                                                DataResult::success
+                                        )
+                                        .forGetter(p_300975_ -> p_300975_.ingredients)
+                        )
+                        .apply(p_311734_, CardClearRecipe::new)
+        );
 
+        @Override
+        public Codec<CardClearRecipe> codec() {
+            return CODEC;
+        }
+
+        public CardClearRecipe fromNetwork(FriendlyByteBuf pBuffer) {
+            String s = pBuffer.readUtf();
+            CraftingBookCategory craftingbookcategory = pBuffer.readEnum(CraftingBookCategory.class);
+            int i = pBuffer.readVarInt();
+            NonNullList<Ingredient> nonnulllist = NonNullList.withSize(i, Ingredient.EMPTY);
+
+            for (int j = 0; j < nonnulllist.size(); ++j) {
+                nonnulllist.set(j, Ingredient.fromNetwork(pBuffer));
+            }
+
+            ItemStack itemstack = pBuffer.readItem();
+            return new CardClearRecipe(s, craftingbookcategory, itemstack, nonnulllist);
+        }
+
+        public void toNetwork(FriendlyByteBuf pBuffer, CardClearRecipe pRecipe) {
+            pBuffer.writeUtf(pRecipe.group);
+            pBuffer.writeEnum(pRecipe.category);
+            pBuffer.writeVarInt(pRecipe.ingredients.size());
+
+            for (Ingredient ingredient : pRecipe.ingredients) {
+                ingredient.toNetwork(pBuffer);
+            }
+
+            pBuffer.writeItem(pRecipe.result);
+        }
     }
 }
